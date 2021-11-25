b1df937605c54fcf342383611d0a124b91497035
조정연 <jeongyeon1224@gmail.com>
2021-11-25 17:15:22 +0900

    base setting

---
+++ board/db_connection.py
@@ -0,0 +1,72 @@
+import pymysql
+import logging
+import sys
+
+class db_conn:
+
+    host = "192.168.229.199"
+    port = 3306
+    database = "dev_board"
+    username = "test"
+    password = "1234"
+
+    # DB 연결 함수
+    def db_connect(self):
+
+        try:
+            conn = pymysql.connect(
+                host=self.host,
+                user=self.username,
+                password=self.password,
+                db=self.database,
+                use_unicode=True,
+                charset="utf8"
+            )
+            return conn
+
+        except:
+            logging.error("장비를 정지합니다.")
+            sys.exit(1)
+
+    def cursor(self, conn):
+        cur = conn.cursor()
+
+        return cur
+
+    ##############################################
+    def select(self, sql):
+
+        conn = self.db_connect()
+        cur = self.cursor(conn)
+        cur.execute(sql)
+        rows = cur.fetchall()
+        conn.close()
+
+        return rows
+
+    ##############################################
+    def insert(self, sql):
+
+        conn = self.db_connect()
+        cur = self.cursor(conn)
+        cur.execute(sql)
+        conn.commit()
+        conn.close()
+
+    ##############################################
+    def update(self, sql):
+
+        conn = self.db_connect()
+        cur = self.cursor(conn)
+        cur.execute(sql)
+        conn.commit()
+        conn.close()
+
+    ##############################################
+    def delete(self, sql):
+
+        conn = self.db_connect()
+        cur = self.cursor(conn)
+        cur.execute(sql)
+        conn.commit()
+        conn.close()
---
+++ main.py
@@ -0,0 +1,26 @@
+import pymysql
+from flask import Flask, render_template
+import board.db_connection as dbc
+
+dbc = dbc.db_conn()
+
+#Flask 객체 인스턴스 생성
+app = Flask(__name__)
+
+@app.route('/') # 접속하는 url
+def index():
+    return render_template('index.html')
+
+@app.route('/db') # db 가져와서 보여주기
+def db():
+    sql = 'delete from board where b_num=2;'
+    dbc.delete(sql)
+
+    sql = 'select * from board;'
+    re = dbc.select(sql)
+    return render_template('db.html', result=re)
+
+if __name__=="__main__":
+    # app.run(debug=True)
+    # host 등을 직접 지정하고 싶다면
+    app.run(host="0.0.0.0", port="5000", debug=True)
