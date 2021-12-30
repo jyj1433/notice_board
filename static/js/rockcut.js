cd92ca32b156a45abbaf0c08310beaeff20aabf2
midniss <kin1829@naver.com>
2021-12-30 19:04:50 +0900

    돌깍 감소 추가

--- static/js/rockcut.js
+++ static/js/rockcut.js
@@ -1,5 +1,6 @@
 var firstIndex = 1;
 var seccondIndex = 101;
+var therdIndex = 201;
 
 function randomNum(min, max){
 var randNum = Math.floor(Math.random()*(max-min+1)) + min;
@@ -49,5 +50,28 @@ function clicker2(){
  }
 
  seccondIndex++
+
+}
+
+function clicker3(){
+ var id = document.getElementById(therdIndex)
+ var per = document.getElementById("percentage").innerText
+ var random = randomNum(0,100)
+ if(random <= parseInt(per)){
+ id.src = "static/image/감소성공.PNG"
+    if(25 == parseInt(per)){}
+    else{
+      document.getElementById("percentage").innerText = parseInt(per)-10
+        }
+ }
+ else{
+    id.src = "static/image/실패.PNG"
+    if(75 == parseInt(per)){}
+    else{
+       document.getElementById("percentage").innerText = parseInt(per)+10
+        }
+ }
+
+ therdIndex++
 
 }
